f09c362d037bade2fed2eabdad147a606c30a81b
kimanziplacide <p.imanzi@alustudent.com>
2024-03-27 13:49:46 +0200

    pushing final codeof splitwise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-/* eslint-disable */
+import { useState } from 'react';
+
 const initialFriends = [
   {
     id: 118836,
@@ -21,10 +21,18 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
   const [friends, setFriends] = useState(initialFriends);
-  const [billName, setBillName] = useState('a friend');
   const [showAddFriend, setShowAddFriend] = useState(false);
+  const [selectedFriend, setSelectedFriend] = useState(null);
 
   function handleShowAddFriend() {
     setShowAddFriend((show) => !show);
@@ -34,118 +42,196 @@ export default function App() {
     setFriends((friends) => [...friends, friend]);
     setShowAddFriend(false);
   }
+
+  function handleSelection(friend) {
+    // setSelectedFriend(friend);
+    setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+    setShowAddFriend(false);
+  }
+
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+
+    setSelectedFriend(null);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendList friends={friends} onclick={setBillName} />
-        {showAddFriend && <FormAddFriend handleAddFriend={handleAddFriend} />}
-        <Button onclick={handleShowAddFriend}>
-          {showAddFriend ? 'Close' : 'Add'}
+        <FriendsList
+          friends={friends}
+          selectedFriend={selectedFriend}
+          onSelection={handleSelection}
+        />
+
+        {showAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? 'Close' : 'Add friend'}
         </Button>
       </div>
-      <FormSplitBill billName={billName} />
+
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
+        />
+      )}
     </div>
   );
 }
 
-function FriendList({ friends, onclick }) {
+function FriendsList({ friends, onSelection, selectedFriend }) {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id} onclick={onclick} />
+        <Friend
+          friend={friend}
+          key={friend.id}
+          selectedFriend={selectedFriend}
+          onSelection={onSelection}
+        />
       ))}
     </ul>
   );
 }
-function Button({ children, onclick }) {
-  return (
-    <button className="button" onClick={onclick}>
-      {children}
-    </button>
-  );
-}
 
-function Friend({ friend, onclick }) {
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
+
   return (
-    <li>
-      <img src={friend.image} alt={friend.name}></img>
+    <li className={isSelected ? 'selected' : ''}>
+      <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
+
       {friend.balance < 0 && (
         <p className="red">
-          You owe {friend.balance} {friend.name}
+          You owe {friend.name} {Math.abs(friend.balance)}€
         </p>
       )}
-
       {friend.balance > 0 && (
         <p className="green">
-          {friend.name} owes you {friend.balance}
+          {friend.name} owes you {Math.abs(friend.balance)}€
         </p>
       )}
-
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <Button onclick={() => onclick(friend.name)}>Select</Button>
+      <Button onClick={() => onSelection(friend)}>
+        {isSelected ? 'Close' : 'Select'}
+      </Button>
     </li>
   );
 }
 
-function FormAddFriend({ handleAddFriend }) {
+function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState('');
   const [image, setImage] = useState('https://i.pravatar.cc/48');
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!name || !image) return;
+
     const id = crypto.randomUUID();
-    const newItem = {
+    const newFriend = {
       id,
       name,
-      image: `${image}?u=${id}`,
+      image: `${image}?=${id}`,
       balance: 0,
     };
 
-    handleAddFriend(newItem);
+    onAddFriend(newFriend);
+
+    setName('');
+    setImage('https://i.pravatar.cc/48');
   }
+
   return (
-    <form onSubmit={handleSubmit} className="form-add-friend">
-      <label>🧑‍🤝‍🧑Friend Name</label>
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>👫 Friend name</label>
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
-      ></input>
+      />
 
-      <label>🙍Image URL</label>
+      <label>🌄 Image URL</label>
       <input
         type="text"
         value={image}
-        onChange={(e) => setImage('https://i.pravatar.cc/48')}
-      ></input>
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
   );
 }
 
-function FormSplitBill({ billName }) {
+function FormSplitBill({ onSplitBill, selectedFriend }) {
+  const [bill, setBill] = useState('');
+  const [userExpense, setuserExpense] = useState('');
+  const friendExpense =
+    isNaN(bill) || bill === 0 || !bill ? '' : bill - userExpense;
+  const [whoPaid, setWhoPaid] = useState('user');
   return (
-    <form className="form-split-bill">
-      <h2>Split a bill with {billName}</h2>
+    <form
+      className="form-split-bill"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (!bill || !userExpense) return;
+        onSplitBill(whoPaid === 'user' ? friendExpense : -userExpense);
+      }}
+    >
+      <h2>Split a bill with {selectedFriend.name}</h2>
 
-      <label>💰Bill Value</label>
-      <input type="text"></input>
+      <label>💰 Bill value</label>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => {
+          if (!isNaN(e.target.value)) {
+            setBill(Number(e.target.value));
+          } else {
+            setBill('wrong input'); // Set 'wrong input' if the input is not a number
+            setTimeout(() => setBill(''), 2000); // Clear the message after 2 seconds
+          }
+        }}
+      />
 
-      <label>🧍 Your expense</label>
-      <input type="text"></input>
+      <label>🧍‍♀️ Your expense</label>
+      <input
+        type="text"
+        value={userExpense}
+        onChange={(e) => {
+          const inputValue = e.target.value;
+          if (!isNaN(inputValue)) {
+            setuserExpense(
+              Number(inputValue) > bill ? userExpense : Number(inputValue)
+            );
+          } else {
+            setuserExpense('wrong input'); // Set 'wrong input' if the input is not a number
+            setTimeout(() => setuserExpense(''), 2000); // Clear the message after 2 seconds
+          }
+        }}
+      />
 
-      <label>🧑‍🤝‍🧑 {billName} expense</label>
-      <input type="text" disabled></input>
+      <label>👫 {selectedFriend.name}'s expense</label>
+      <input type="text" value={friendExpense} disabled />
 
-      <label>🤑Who is paying</label>
-      <select>
+      <label>🤑 Who is paying the bill</label>
+      <select value={whoPaid} onChange={(e) => setWhoPaid(e.target.value)}>
         <option value="user">You</option>
-        <option value="friend">{billName}</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
-      <Button>Split Bill</Button>
+
+      <Button>Split bill</Button>
     </form>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js';
-const root = ReactDom.createRoot(document.getElementById('root'));
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
